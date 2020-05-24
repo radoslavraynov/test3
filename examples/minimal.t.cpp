cdc9e0847c8bf9b0a8ab8aa90caf8aa046f288be
Radoslav <radoslav@math.cornell.edu>
2020-05-24 16:34:03 -0400

    Saturday commit

---
+++ examples/minimal.t.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Radoslav Zlatev on 5/24/20.
+//
+
+#include <gmp.h>
+#include <gtest/gtest.h>
+
+TEST(Cxx17, Basic)
+{
+    std::unordered_map<int,int> mp;
+    for(int i=0;i<10;++i)
+        mp[i] = i*i;
+    for(const auto& [k, v]: mp)
+        EXPECT_EQ(k*k, v);
+}
+
+TEST(GmpTest, Basic)
+{
+    printf("this is a test\n");
+
+    mpz_t a, b;
+    mpz_init(a);
+    mpz_init_set_str(a, "12345678901234567890", 10);
+    gmp_fprintf(stderr, "and this is an gmp int %Zd\n", a);
+    mpz_init(b);
+    mpz_pow_ui(b,a,10);
+
+    const char* b_str = mpz_get_str(nullptr, 10, b);
+    EXPECT_STREQ("82252625996962883902200053988119600331774131569207631446528559806647083663566037435187912750978162286251755705030444941948763304746218157802392139307392345500269796500763077997366010000000000", b_str);
+    EXPECT_EQ(191, strlen(b_str));
+}
