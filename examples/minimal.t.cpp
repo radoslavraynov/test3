8a7bc94abb186b1ebf513e1abf26d14a150f3fa6
Radoslav <radoslav@math.cornell.edu>
2020-05-25 00:28:51 -0400

    Try building with github actions

--- examples/minimal.t.cpp
+++ examples/minimal.t.cpp
@@ -17,14 +17,12 @@ TEST(Cxx17, Basic)
 
 TEST(GmpTest, Basic)
 {
-    printf("this is a test\n");
-
     mpz_t a, b;
     mpz_init(a);
     mpz_init_set_str(a, "12345678901234567890", 10);
-    gmp_fprintf(stderr, "and this is an gmp int %Zd\n", a);
     mpz_init(b);
     mpz_pow_ui(b,a,10);
+    gmp_fprintf(stderr, "and this is an gmp int %Zd\n", b);
 
     const char* b_str = mpz_get_str(nullptr, 10, b);
     EXPECT_STREQ("82252625996962883902200053988119600331774131569207631446528559806647083663566037435187912750978162286251755705030444941948763304746218157802392139307392345500269796500763077997366010000000000", b_str);
